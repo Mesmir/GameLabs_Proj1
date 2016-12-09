f39426e558459a5bc68e62031eb43b2d8115fef8
SunrayF <Sunray-muijsson@hotmail.com>
2016-12-09 15:47:20 +0100

    Levels in elkaar gezet voor een gedeelte

---
+++ Project/UnityProject_GameLabs1/Assets/Workmap/Sunray(art assets)/Lights.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lights : MonoBehaviour {
+
+	[Range(0,1000)]
+	public float intensity;
+	public Light lt;
+	void Start()
+	{
+		lt = GetComponent<Light>();
+	}
+	void Update()
+	{
+		lt.intensity = intensity;
+	}
+}
