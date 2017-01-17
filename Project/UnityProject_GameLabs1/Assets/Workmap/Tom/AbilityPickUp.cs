2fbffe369a92bb9b8f67a0fb8fa6309a1f36ba8b
Tomvhe <tvhe@outlook.com>
2017-01-17 15:29:08 +0100

    Added Ability Pickup
    Added a pickup giving you the dash ability

---
+++ Project/UnityProject_GameLabs1/Assets/Workmap/Tom/AbilityPickUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityPickUp : MonoBehaviour {
+
+    private GameObject script;
+    public GameObject image;
+
+    private void Start()
+    {
+        script = GameObject.FindWithTag("Player");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            script.GetComponent<Ability>().enabled = true;
+            image.SetActive(false);
+            Destroy(gameObject);
+        }
+    }
+}
