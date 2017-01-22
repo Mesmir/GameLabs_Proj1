0e2817e3a1631c3e7833c585596416c564a03a8f
SunrayF <Sunray-muijsson@hotmail.com>
2017-01-22 22:47:19 +0100

    not sure if i have changes.

---
+++ Project/UnityProject_GameLabs1/Assets/Workmap/Sunray(art assets)/Code/MoveGate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveGate : MonoBehaviour {
+
+	public GameObject target;
+	private float ogPosX, ogPosY, ogPosZ;
+	public float amountUp;
+
+	// Use this for initialization
+	void Start () {
+		ogPosX = target.transform.position.x;
+		ogPosY = target.transform.position.y;
+		ogPosZ = target.transform.position.z;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		target.transform.position = new Vector3 (ogPosX, ogPosY + amountUp, ogPosZ);
+	}
+}
